6a47ad1082857977b4681e4d27b2f1319bf8a2d0
habdmouleh <habdmouleh@vermeg.com>
2021-07-04 22:38:21 +0100

    fix(login): add formControlName

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,33 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  private loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    let formControls = {
+      cin: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2),
+      ]),
+    }
+    this.loginForm = this.formBuilder.group(formControls);
+  }
+  get cin(){return this.cin.get('cin')}
+  get password(){return this.password.get('password')}
 
   ngOnInit() {
   }
   ngOnDestroy() {
+  }
+
+  login(){
+    console.log(this.loginForm.value)
   }
 
 }
