06aecf75dbb9a55e6b7a74397091403b62405df5
habdmouleh <habdmouleh@vermeg.com>
2021-07-08 17:38:07 +0100

    fix(table):update screen

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -18,14 +18,16 @@ import { MaterialService } from "src/app/services/material.service";
 })
 export class TablesComponent implements OnInit {
   public medecines: Medecine[];
-  private thead: String[];
-  private theadList: String[];
+  thead;
+  theadList;
   entity: string;
   doctors: RegisterUser[];
   patient: RegisterUser[];
   disease: Disease[];
   list;
   service: string;
+  rows;
+  headers;
   constructor(
     private route: ActivatedRoute,
     private medecineService: MedecineService,
@@ -41,74 +43,83 @@ export class TablesComponent implements OnInit {
     this.entity = this.route.snapshot.paramMap.get("model");
     switch (this.entity) {
       case "medecines": {
-        this.theadList = [
-          "ID",
-          "Reference",
-          "Manufacturer",
-          "Quantity",
-          "Expiration date",
-          "Price",
-        ];
         this.medecineService
           .allMedecines()
           .subscribe((medecineList: Medecine[]) => {
-            this.list = medecineList;
+            this.rows = medecineList;
           });
+
+        this.headers = [
+          { label: "ID", value: "id" },
+          { label: "Reference", value: "reference" },
+          { label: "Manufacturer", value: "manufacturer" },
+          { label: "Quantity", value: "quantity" },
+          { label: "Expiration date", value: "expirationDate" },
+          { label: "Price", value: "price" },
+        ];
       }
       case "doctor": {
-        this.theadList = [
-          "FirstName",
-          "LastName",
-          "Serial Number",
-          "gender",
-          "Hosting Hospital",
-          "country",
+        this.headers = [
+          { label: "FirstName", value: "firstName" },
+          { label: "LastName", value: "lastName" },
+          { label: "Serial Number", value: "serialNumber" },
+          { label: "Gender", value: "gender" },
+          { label: "Hosting Hospital", value: "hostingHospital" },
+          { label: "Country", value: "country" },
         ];
         this.doctorService.allDoctor().subscribe((doctor: RegisterUser[]) => {
-          this.list = doctor;
+          this.rows = doctor;
         });
       }
-      case "patient": {
-        this.theadList = [
-          "FirstName",
-          "LastName",
-          "Serial Number",
-          "gender",
-          "Hosting Hospital",
-          "country",
+      case "patients": {
+        this.headers = [
+          { label: "FirstName", value: "firstName" },
+          { label: "LastName", value: "lastName" },
+          { label: "Serial Number", value: "serialNumber" },
+          { label: "Gender", value: "gender" },
+          { label: "Country", value: "country" },
         ];
         this.patientService
           .allPatient()
           .subscribe((patient: RegisterUser[]) => {
-            this.list = patient;
+            this.rows = patient;
           });
       }
       case "diseases": {
-        this.theadList = ["ID", "Name", "Description", "Medecines"];
+        this.headers = [
+          { label: "ID", value: "id" },
+          { label: "Name", value: "name" },
+          { label: "Description", value: "description" },
+          { label: "Medecines", value: "medecines" },
+        ];
         this.diseaseService.allDiseases().subscribe((disease: Disease[]) => {
-          this.list = disease;
+          this.rows = disease;
         });
         break;
       }
-      case "medicalRecord": {
-        this.theadList = [
-          "PatientFirstName",
-          "PatientLastName",
-          "PatientGender",
-          "PatientDiseases",
+      case "medicalRecords": {
+        this.headers = [
+          { label: "PatientFirstName", value: "firstName" },
+          { label: "PatientLastName", value: "lastName" },
+          { label: "PatientDiseases", value: "disease" },
+          { label: "PatientGender", value: "gender" },
         ];
         this.medicalRecordService
           .allMedicalRecord()
           .subscribe((patient: RegisterUser[]) => {
-            this.patient = patient;
+            this.rows = patient;
           });
       }
       case "material": {
-        this.theadList = ["Name", "quantity", "price"];
+        this.headers = [
+          { label: "Name", value: "name" },
+          { label: "Quantity", value: "quantity" },
+          { label: "Price", value: "price" },
+        ];
         this.materialService
           .allMaterial()
           .subscribe((patient: RegisterUser[]) => {
-            this.list = patient;
+            this.rows = patient;
           });
       }
       case "Another": {
@@ -144,16 +155,18 @@ export class TablesComponent implements OnInit {
         });
         break;
       }
-      case "diseases": {
+      case "disease": {
         this.diseaseService.deleteMedecine(source.id).subscribe((res: any) => {
           console.log(res);
         });
         break;
       }
       case "medicalRecord": {
-        this.medicalRecordService.deleteMedecine(source.id).subscribe((res: any) => {
-          console.log(res);
-        });
+        this.medicalRecordService
+          .deleteMedecine(source.id)
+          .subscribe((res: any) => {
+            console.log(res);
+          });
         break;
       }
       case "material": {
@@ -168,7 +181,7 @@ export class TablesComponent implements OnInit {
     this.router.navigate([`form/${screen}/${id}`]);
   }
   add(screen) {
-    console.log()
+    console.log();
     this.router.navigate([`form/${screen}`]);
   }
 }
